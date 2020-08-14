5c001856b8b0f9275fe3626410bdd0b88c2a58e6
mejgun <mejgun@gmail.com>
2020-08-14 14:36:04 +0300

    add url parser test

---
+++ parser_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+var testPairs = map[string][3]string{
+	"/play/youtu.be/jNQXAC9IVRw?/?vh=360&vf=mp4": {"youtu.be/jNQXAC9IVRw", "360", defaultVideoFormat},
+	"/play/youtu.be/jNQXAC9IVRw?/?vh=720?vf=avi": {"youtu.be/jNQXAC9IVRw", "720", defaultVideoFormat},
+	"/play/youtu.be/jNQXAC9IVRw":                 {"youtu.be/jNQXAC9IVRw", defaultVideoHeight, defaultVideoFormat},
+}
+
+func TestParseQuery(t *testing.T) {
+	for k, v := range testPairs {
+		if u, h, f := parseQuery(k); [3]string{u, h, f} != v {
+			t.Error("For", k, "expected", v, "got", u, h, f)
+		}
+	}
+}
