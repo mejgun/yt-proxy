4f5ae1b1524c7c7dbdd5c23da695b24871c44326
mejgun <mejgun@gmail.com>
2020-08-14 15:31:52 +0300

    build script edit

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 # https://github.com/golang/go/blob/master/src/go/build/syslist.go
 
-OSLIST="aix android darwin dragonfly freebsd hurd illumos js linux nacl netbsd openbsd plan9 solaris windows zos"
-ARCHLIST="386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm"
+OSListStr="aix android darwin dragonfly freebsd hurd illumos js linux nacl netbsd openbsd plan9 solaris windows zos"
+OSList=($OSListStr)
 
-for OS in ${OSLIST}
+ArchListStr="386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm"
+ArchList=($ArchListStr)
+
+Total=$((${#OSList[@]}*${#ArchList[@]}))
+
+for OS in ${OSListStr}
 do
-    for ARCH in ${ARCHLIST}
+    for ARCH in ${ArchListStr}
     do
-    echo ${OS}/${ARCH}
-    GOPATH=$(pwd) GOOS=${OS} GOARCH=${ARCH} go build -ldflags '-s -w' -o bin/yt-proxy-${OS}-${ARCH} > /dev/null 2>&1
+        echo $Total ${OS}/${ARCH}
+        GOPATH=$(pwd) GOOS=${OS} GOARCH=${ARCH} go build -ldflags '-s -w' -o bin/yt-proxy-${OS}-${ARCH} > /dev/null 2>&1
+        ((Total=Total-1))
     done
 done
