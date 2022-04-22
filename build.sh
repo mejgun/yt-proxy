cfe13185cc513fdae383343894bf14ab2ea3c910
mejgun <mejgun@gmail.com>
2022-04-22 18:34:59 +0300

    move go sources to src/

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 
 GoBin=go
 
-BinDir="bin"
+BinDir="../bin"
 FilePrefix="yt-proxy"
 
-OSListStr="aix android darwin dragonfly freebsd hurd illumos ios js linux nacl netbsd openbsd plan9 solaris windows zos"
+OSListStr="aix android darwin dragonf freebsd hurd illumos ios js linux nacl netbsd openbsd plan9 solaris windows zos"
 OSList=($OSListStr)
 
-ArchListStr="386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le loong64 mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm"
+ArchListStr="386 amd64 amd64p32 arm armbe arm64 arm64be loong64 mips mipsle mips64 mips64le mips64p32 mips64p32 ppc ppc64 ppc64le riscv riscv64 s390 s390x sparc sparc64 wasm"
 ArchList=($ArchListStr)
 
 Total=$((${#OSList[@]} * ${#ArchList[@]}))
@@ -20,11 +20,12 @@ date >${Md5File}
 
 mkdir -p ${BinDir} || exit
 rm ${BinDir}/${FilePrefix}* -rf || exit
+cd src || exit
 for OS in ${OSListStr}; do
     for ARCH in ${ArchListStr}; do
         echo $Total ${OS}/${ARCH}
         File=${FilePrefix}-${OS}-${ARCH}
-        GOOS=${OS} GOARCH=${ARCH} $GoBin build -ldflags '-s -w' -o ${BinDir}/${File} >/dev/null 2>&1 && cd ${BinDir} && md5sum ${File} >>../${Md5File} && cd ..
+        GOOS=${OS} GOARCH=${ARCH} $GoBin build -ldflags '-s -w' -o ${BinDir}/${File} >/dev/null 2>&1 && cd ${BinDir} && md5sum ${File} >>../${Md5File} && cd -
         ((Total = Total - 1))
     done
 done
