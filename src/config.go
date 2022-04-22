991c91dba5fc27dcba4dcf9573eec7dc9b47d290
mejgun <mejgun@gmail.com>
2022-04-22 19:21:52 +0300

    start moving extractor to config

--- src/config.go
+++ src/config.go
@@ -1,11 +1,18 @@
 package main
 
 type configT struct {
-	EnableDebug          bool     `json:"debug"`
-	EnableErrorHeaders   bool     `json:"error-headers"`
-	IgnoreMissingHeaders bool     `json:"ignore-missing-headers"`
-	IgnoreSSLErrors      bool     `json:"ignore-ssl-errors"`
-	PortInt              uint16   `json:"port"`
-	ErrorVideoPath       string   `json:"error-video"`
-	Extractor            struct{} `json:"extractor"`
+	EnableDebug          bool       `json:"debug"`
+	EnableErrorHeaders   bool       `json:"error-headers"`
+	IgnoreMissingHeaders bool       `json:"ignore-missing-headers"`
+	IgnoreSSLErrors      bool       `json:"ignore-ssl-errors"`
+	PortInt              uint16     `json:"port"`
+	ErrorVideoPath       string     `json:"error-video"`
+	Extractor            extractorT `json:"extractor"`
+}
+
+type extractorT struct {
+	Path         string   `json:"path"`
+	MP4          []string `json:"mp4"`
+	M4A          []string `json:"m4a"`
+	GetUserAgent []string `json:"get-user-agent"`
 }
