e3132a754da6bad5357e9435f7a9cdf830e80370
mejgun <mejgun@gmail.com>
2022-04-25 15:41:18 +0300

    build scripts rework

---
+++ build.go
@@ -0,0 +1,87 @@
+// DO NOT RUN DIRECTLY
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const (
+	binDir     = "bin"
+	filePrefix = "yt-proxy"
+	goBin      = "go"
+	myos       = "linux"
+	myarch     = "amd64"
+)
+
+func main() {
+	total := len(knownArch) * len(knownOS)
+	for os_ := range knownOS {
+		for arch := range knownArch {
+			file := fmt.Sprintf("%s-%s-%s", filePrefix, os_, arch)
+			filePath := fmt.Sprintf("../%s/%s", binDir, file)
+			fmt.Printf("%d %-40s ", total, file)
+			cmd := exec.Command(goBin, "build", "-o", filePath, "-ldflags", "-s -w")
+			cmd.Env = os.Environ()
+			if myos == os_ && myarch == arch {
+				cmd.Env = append(cmd.Env, "CGO_ENABLED=0")
+			}
+			cmd.Env = append(cmd.Env, fmt.Sprintf("GOOS=%s", os_))
+			cmd.Env = append(cmd.Env, fmt.Sprintf("GOARCH=%s", arch))
+			if err := cmd.Run(); err == nil {
+				fmt.Print("OK")
+			}
+			fmt.Println()
+			total--
+		}
+	}
+}
+
+var knownOS = map[string]bool{
+	"aix":       true,
+	"android":   true,
+	"darwin":    true,
+	"dragonfly": true,
+	"freebsd":   true,
+	"hurd":      true,
+	"illumos":   true,
+	"ios":       true,
+	"js":        true,
+	"linux":     true,
+	"nacl":      true,
+	"netbsd":    true,
+	"openbsd":   true,
+	"plan9":     true,
+	"solaris":   true,
+	"windows":   true,
+	"zos":       true,
+}
+
+var knownArch = map[string]bool{
+	"386":         true,
+	"amd64":       true,
+	"amd64p32":    true,
+	"arm":         true,
+	"armbe":       true,
+	"arm64":       true,
+	"arm64be":     true,
+	"loong64":     true,
+	"mips":        true,
+	"mipsle":      true,
+	"mips64":      true,
+	"mips64le":    true,
+	"mips64p32":   true,
+	"mips64p32le": true,
+	"ppc":         true,
+	"ppc64":       true,
+	"ppc64le":     true,
+	"riscv":       true,
+	"riscv64":     true,
+	"s390":        true,
+	"s390x":       true,
+	"sparc":       true,
+	"sparc64":     true,
+	"wasm":        true,
+}
--- build.sh
+++ build.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-# https://github.com/golang/go/blob/master/src/go/build/syslist.go
-
-GoBin=go
-
-BinDir="../bin"
-FilePrefix="yt-proxy"
-
-OSListStr="aix android darwin dragonf freebsd hurd illumos ios js linux nacl netbsd openbsd plan9 solaris windows zos"
-OSList=($OSListStr)
-
-ArchListStr="386 amd64 amd64p32 arm armbe arm64 arm64be loong64 mips mipsle mips64 mips64le mips64p32 mips64p32 ppc ppc64 ppc64le riscv riscv64 s390 s390x sparc sparc64 wasm"
-ArchList=($ArchListStr)
-
-Total=$((${#OSList[@]} * ${#ArchList[@]}))
+BinDir="bin"
 Md5File="all.md5"
 
 date >${Md5File}
-
 mkdir -p ${BinDir} || exit
 rm ${BinDir}/${FilePrefix}* -rf || exit
 cd src || exit
-for OS in ${OSListStr}; do
-    for ARCH in ${ArchListStr}; do
-        echo $Total ${OS}/${ARCH}
-        File=${FilePrefix}-${OS}-${ARCH}
-        GOOS=${OS} GOARCH=${ARCH} $GoBin build -ldflags '-s -w' -o ${BinDir}/${File} >/dev/null 2>&1 && cd ${BinDir} && md5sum ${File} >>../${Md5File} && cd -
-        ((Total = Total - 1))
-    done
+go run ../build.go || exit
+cd ../$BinDir || exit
+for i in $(ls -1 *); do
+    md5sum $i >>../${Md5File} || exit
 done
+cd ..
